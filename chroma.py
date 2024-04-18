c3f5e348626babc61f41410fe4cabd1b674cd39c
BlankAdventure <>
2024-04-18 11:19:45 -0400

    clean up chroma script

--- chroma.py
+++ chroma.py
@@ -5,9 +5,15 @@ Created on Wed Apr 17 16:12:02 2024
 @author: BlankAdventure
 """
 
+# *****************************************************************************
+# Example script for building a chromadb embeddings vector database from
+# from a specified YouTube channel
+# *****************************************************************************
+
 import videos
 import chromadb
 
+# Name of channel to process 
 COLLECTION_NAME = "MrCarlsonsLab"
 PATH = "db/"+COLLECTION_NAME
 
@@ -16,19 +22,7 @@ collection = client.get_or_create_collection(name=COLLECTION_NAME)
 
 vid_ids = videos.get_video_ids(channel=COLLECTION_NAME)
 
-# for testing just want a few entries
-
 videos.add_to_db(vid_ids, collection)
 
-
-#results = collection.query(query_texts=['shielding'])
-
-# docs1=["This is a document about cat"]
-# meta1=[{"category": "animal"}]
-# ids1=["id1"]
-
-# collection.add(documents=docs1, metadatas=meta1, ids=ids1)
-
-# docs2=["a thing about dogs"]
-# meta2=[{"category": "animal"}]
-# ids2=["id2"]
+# Example query
+results = collection.query(query_texts=['how to solve noise issues?'])
