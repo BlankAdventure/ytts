e2c1bc400e8486cbafa2167a4f46ed64d5bf5355
BlankAdventure <>
2024-04-19 13:53:39 -0400

    Add chunk generator (combines sentences together in rolling window sense

--- videos.py
+++ videos.py
@@ -4,7 +4,7 @@ Created on Tue Apr  2 17:28:03 2024
 
 @author: BlankAdventure
 """
-
+import time
 import scrapetube
 from youtube_transcript_api import YouTubeTranscriptApi
 from youtube_transcript_api.formatters import TextFormatter
@@ -12,7 +12,45 @@ import urllib.request
 import json
 import urllib
 from tqdm import tqdm
+from math import ceil
+
 tqdm._instances.clear()
+
+
+# Chunks together transcript rows into buff_len, and advances by adv_by. 
+# buff_len: number of sentences to combine
+# adv_by: number of sentences to advancy by (think sliding window)
+def chunk_generator(vid_ids: list[str], buff_len: int, adv_by: int) -> dict | None:
+    NV = len(vid_ids)
+    def get_chunk(a, b):
+        buffer = transcript[a:b]    
+        text = get_text(buffer)
+        metadata = {'timestamp': buffer[0]['start'], 'title': title, 'video': vid}
+        uid = f'{vid}_{start}'
+        return {'text': text, 'metadata': metadata, 'uid': uid}
+
+    for n, vid in enumerate(vid_ids):
+        start = 0
+        stop = buff_len 
+        title = get_video_metadata(vid)['title'] #for now we only care about the title
+        transcript = get_transcript(vid)  
+        if transcript:
+            NT = len(transcript)
+            P = ceil( (NT-buff_len)/adv_by )
+            pbar = tqdm(total=P,desc=f'{vid} ({n+1}/{NV})')
+            while stop < NT:
+                yield get_chunk(start, stop)
+                time.sleep(0.1)
+                start += adv_by
+                stop += adv_by
+                pbar.update(1)
+            # Need to get any leftovers...
+            yield ( get_chunk(start,NT) )
+            pbar.update(1)
+            pbar.close()                
+        else:
+            yield None
+
 
 # Returns a generator yielding either a dictionary containing the transcript text, 
 # metadata and unique ID, OR, None if unable to load. 
@@ -77,8 +115,7 @@ def get_transcript(vid_id: str) -> dict | None:
        
 # Convert transcript (list of dicts featuring sub-strings) into single
 # (unbroken) text string. All metadata is discarded!
-def get_text(vid_id: str, strip: bool = True) -> str:
-    transcript = YouTubeTranscriptApi.get_transcript(vid_id)
+def get_text(transcript: list[dict], strip: bool = True) -> str:
     text = TextFormatter().format_transcript(transcript)
     if strip:
         text = [s for s in text.replace("\n"," ") if s.isalnum() or s.isspace()]
