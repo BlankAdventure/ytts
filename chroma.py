e6c73cef453ad25556c1010741438411b1b0bb59
BlankAdventure <>
2024-04-18 23:45:18 -0400

    Account for relative path in build_db

--- chroma.py
+++ chroma.py
@@ -27,12 +27,12 @@ def add_to_db(vid_ids: list[str], collection) -> list[str]:
 # Top-level function for building a chromadb embeddings vector database from
 # a specified YouTube channel. The channel name should be the part after the 
 # @ symbol
-def build_db(channel_name: str):
-    path = "db/"+channel_name
-    full_path = os.path.normpath(os.path.join(os.getcwd(),path))
+def build_db(channel_name: str, path: str = '.'):
+    full_path = os.path.normpath(os.path.join(os.path.abspath(path),channel_name))
     print(f'SAVING TO: {full_path}') #show full path to stored db
-    client = chromadb.PersistentClient(path=path)
+    client = chromadb.PersistentClient(path=full_path)
     collection = client.get_or_create_collection(name=channel_name)    
+    print('Getting video list...')
     vid_ids = videos.get_video_ids(channel=channel_name)
     add_to_db(vid_ids, collection) #modify-in-place operation
     return collection
