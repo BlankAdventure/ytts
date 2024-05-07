2637e2b157e0a793de5b69f286f69bf2eb591baa
BlankAdventure <>
2024-05-07 17:15:39 -0400

    remove appending channel name to db location; script builds single database with 2 collections

--- build_script.py
+++ build_script.py
@@ -5,17 +5,17 @@ Created on Wed May  1 12:48:18 2024
 
 """
 
-# Script for generating our two local ytts databases
+# Script for generating our local ytts database
 
 from chroma import build_db
 
 
 chan_name = 'MrCarlsonslab'
 config = {'channel_name': chan_name,
-          'db_name': chan_name,
+          'collection_name': chan_name,
           'path': "./",
-          'chunk': 8,
-          'adv_by': 5
+          'chunk': 6,
+          'adv_by': 4
           }
 
 build_db(**config)
@@ -23,7 +23,7 @@ build_db(**config)
 
 chan_name = 'Thesignalpath'
 config = {'channel_name': chan_name,
-          'db_name': chan_name,
+          'collection_name': chan_name,
           'path': "./",
           'chunk': 6,
           'adv_by': 4
--- chroma.py
+++ chroma.py
@@ -27,11 +27,11 @@ def add_to_db(vid_ids: list[str], collection, chunk: int, adv_by: int) -> list[s
 # Top-level function for building a chromadb embeddings vector database from
 # a specified YouTube channel. The channel name should be the part after the 
 # @ symbol
-def build_db(channel_name: str, db_name: str, path: str = '.', **kwargs):
-    full_path = os.path.normpath(os.path.join(os.path.abspath(path),channel_name))
-    print(f'SAVING TO: {full_path}') #show full path to stored db
+def build_db(channel_name: str, collection_name: str, path: str = '.', **kwargs):
+    full_path = os.path.normpath(os.path.abspath(path))
+    print(f'SAVING TO: {full_path} <{collection_name}>') #show full path to stored db
     client = chromadb.PersistentClient(path=full_path)
-    collection = client.get_or_create_collection(name=db_name)    
+    collection = client.get_or_create_collection(name=collection_name)    
     print('Getting video list...')
     vid_ids = videos.get_video_ids(channel=channel_name)
     add_to_db(vid_ids, collection, **kwargs) #modify-in-place operation
