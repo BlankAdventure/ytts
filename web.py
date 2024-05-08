a8d577c801f1640a77949ac5c7926bbffe28de00
BlankAdventure <>
2024-05-08 18:42:31 -0400

    add UI overlay; test with persistent db; get_results back to standard def function and wrap in io_bound call.

--- web.py
+++ web.py
@@ -10,6 +10,26 @@ from nicegui import ui, run
 import chromadb
 
 
+class Overlay(ui.element):
+    def __init__(self, message, bg_color=(0,0,0,0.5)):
+        super().__init__(tag='div')
+        self.message = message
+        self.style('position: fixed; display: block; width: 100%; height: 100%;'
+                   'top: 0; left: 0; right: 0; bottom: 0; z-index: 2; cursor: pointer;'
+                   'background-color:' + f'rgba{bg_color};')
+        with self:
+            with ui.element('div').classes("h-screen flex items-center justify-center"):
+                ui.label(self.message).style("font-size: 50px; color: white;")
+        self.hide()
+
+    def show(self):
+        self.set_visibility(True)
+    
+    def hide(self):
+        self.set_visibility(False)
+
+
+# Converts timestamp returned by query into more use-friendly string
 def format_timestamp(timestamp: float) -> str:
     hours = int(timestamp / 3600)
     minutes = int((timestamp % 3600) / 60)
@@ -17,9 +37,9 @@ def format_timestamp(timestamp: float) -> str:
     return f"{hours}h{minutes}m{seconds}s"
 
 # Issue a querty to the database and return (cleaned up) results
-async def get_results(collection, qstr: str, n=5) -> list[dict]:
+def get_results(collection, qstr: str, n=5) -> list[dict]:
     transformed_list = []
-    results = await run.io_bound(collection.query, query_texts=[qstr], n_results=n) 
+    results = collection.query(query_texts=[qstr], n_results=n) 
     remove = ('data', 'uris', 'embeddings') #Don't need these
     for k in remove:
         results.pop(k, None)
@@ -32,14 +52,14 @@ async def get_results(collection, qstr: str, n=5) -> list[dict]:
     return transformed_list
 
 
-async def main(channel_name, db_name, db_path=None):
+async def main(channel_name: str, db_name: str, db_path: str=None) -> None:
     
-    # Load db from persistent media location is specfied
+    # Load db from persistent media location if db_path specfied
+    # otherwise connect to server (running locally)
     if db_path:
         client = chromadb.PersistentClient(path=db_path)
-    else: # otherwise connection to server (running locally)
+    else: # 
         client = chromadb.HttpClient(host="localhost", port=8000)
-        
     collection = client.get_collection(name=db_name)
     
     # Populate the search results panel
@@ -48,7 +68,9 @@ async def main(channel_name, db_name, db_path=None):
         results = []
         query = search.value
         if query:    
-            results = await get_results(collection, query, 5)
+            overlay.show()
+            results = await run.io_bound(get_results,collection, query, 5)
+            overlay.hide()
             if results:
                 ui.label('Results').style('font-size: 125%').classes('bg-slate-300 w-full')
                 for entry in results:
@@ -64,6 +86,7 @@ async def main(channel_name, db_name, db_path=None):
                             ui.label("..."+entry['documents']+"...").style('font-size: 115%; font-style: italic')
     
     # Build the UI
+    overlay = Overlay('Searching. Please be patient.')
     with ui.row().classes('w-full gap-2'):
         with ui.column().classes().style():
             ui.label(f'Search {channel_name}').style('font-size: 125%').classes('w-full text-center bg-slate-300')
@@ -77,11 +100,11 @@ async def main(channel_name, db_name, db_path=None):
 if __name__ in {"__main__", "__mp_main__"}:
     @ui.page('/a')
     async def testa():
-        await (main('MrCarlsonsLab','MrCarlsonslab'))
+        await (main('MrCarlsonsLab','MrCarlsonslab', db_path='Z:/ytts_db'))
 
     @ui.page('/b')
     async def testb():
-        await (main('TheSignalPath','Thesignalpath'))
+        await (main('TheSignalPath','Thesignalpath', db_path='Z:/ytts_db'))
 
     ui.run(title='YTTS')
 
