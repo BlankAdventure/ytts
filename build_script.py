959f665a4d9546a851dd572a9c49111bf67c6ccc
BlankAdventure <>
2024-05-14 17:14:19 -0400

    Comebine both channels into one embedding db; use word chunking (20/15)

--- build_script.py
+++ build_script.py
@@ -8,14 +8,15 @@ Created on Wed May  1 12:48:18 2024
 # Script for generating our local ytts database
 
 from chroma import build_db
-
+from videos import word_chunk_generator
 
 chan_name = 'MrCarlsonslab'
 config = {'channel_name': chan_name,
-          'collection_name': chan_name,
-          'path': "./",
-          'chunk': 6,
-          'adv_by': 4
+          'collection_name': 'word_chunk_20_15',
+          'path': "Z:/ytts_db",
+          'chunk': 20,
+          'adv_by': 15,
+          'chunk_func':  word_chunk_generator
           }
 
 build_db(**config)
@@ -23,10 +24,11 @@ build_db(**config)
 
 chan_name = 'Thesignalpath'
 config = {'channel_name': chan_name,
-          'collection_name': chan_name,
-          'path': "./",
-          'chunk': 6,
-          'adv_by': 4
+          'collection_name': 'word_chunk_20_15',
+          'path': "Z:/ytts_db",
+          'chunk': 20,
+          'adv_by': 15,
+          'chunk_func': word_chunk_generator
           }
           
 build_db(**config)
